c2c6bc424a1b974c25ba68f100a59f75d7f546f1
Michał Boruch <michal.boruch@bettertyped.com>
2022-11-06 11:59:56 +0100

    feat: firebase config

--- src/config/environment.config.ts
+++ src/config/environment.config.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  serverUrl: process.env.REACT_APP_API || "http://12.12.122.67:3000",
+  firebaseApiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  firebaseAuthDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  firebaseProjectId: process.env.REACT_APP_PROJECT_ID,
+  firebaseStorageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  firebaseMessagingSender: process.env.REACT_APP_MESSAGING_SENDER,
+  firebaseAppId: process.env.REACT_APP_FIREBASE_ID,
 };
